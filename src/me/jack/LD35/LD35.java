9e8756f617315e25a2235a5b8ad6645a3303254d
Jack Patrick <jackpatrickmail@gmail.com>
2016-04-16 03:31:17 +0100

    Forgot to add some files

---
+++ src/me/jack/LD35/LD35.java
@@ -0,0 +1,21 @@
+package me.jack.LD35;
+
+import me.jack.LD35.States.InGameState;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+/**
+ * Created by Jack on 16/04/2016.
+ */
+public class LD35 extends StateBasedGame{
+
+    public LD35() {
+        super("XXXX for LD35 - Theme:Shapeshift");
+    }
+
+    @Override
+    public void initStatesList(GameContainer gameContainer) throws SlickException {
+        addState(new InGameState());
+    }
+}
